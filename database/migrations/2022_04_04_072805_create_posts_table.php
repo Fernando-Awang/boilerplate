35bd2b8da84a39d9ab255b199f0efae5445f2a2d
Fernando-Awang <fernandoawang@gmail.com>
2022-04-04 14:44:29 +0700

    migration post

---
+++ database/migrations/2022_04_04_072805_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_category_id');
+            $table->foreignId('author');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('thumbnail');
+            $table->longText('content');
+            $table->timestamps();
+            $table->foreign('post_category_id')->references('id')->on('post_categories');
+            $table->foreign('author')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
